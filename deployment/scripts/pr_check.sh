577c2b0b4f463bdf998d69dca375f342719e9a9d
Katherine Black <kblack@redhat.com>
2021-08-30 15:37:59 -0700

    wip

--- deployment/scripts/pr_check.sh
+++ deployment/scripts/pr_check.sh
@@ -5,7 +5,7 @@ export COMPONENT_NAME="cloudigrade"  # name of app-sre "resourceTemplate" in dep
 export IMAGE="quay.io/cloudservices/cloudigrade"  # the image location on quay
 
 export IQE_PLUGINS="cloudigrade"  # name of the IQE plugin for this APP
-export IQE_MARKER_EXPRESSION="cloudigrade_smoke"  # This is the value passed to pytest -m
+export IQE_MARKER_EXPRESSION="smoke"  # This is the value passed to pytest -m
 export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
 export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
 
@@ -17,10 +17,10 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 source $CICD_ROOT/build.sh
 
 # Run the unit tests with an ephemeral db
-source $APP_ROOT/deployment/scripts/unit_test.sh
+# source $APP_ROOT/deployment/scripts/unit_test.sh
 
-# Deploy rbac to an ephemeral namespace for testing
+# Deploy cloudigrade to an ephemeral namespace for testing
 source $CICD_ROOT/deploy_ephemeral_env.sh
 
 # Run smoke tests with ClowdJobInvocation
-# source $CICD_ROOT/cji_smoke_test.sh
+source $CICD_ROOT/cji_smoke_test.sh
